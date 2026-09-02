f3f4fbaad3f386369d3f12ba67f9f3a914d2325e
fabiodev19-arch <fabiodev19@gmail.com>
2026-09-02 13:10:27 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // internet — inclusive na primeira tela, sem precisar já ter sido aberto
 // online antes de ir a campo.
 
-var CACHE_NAME = "gtm-rdo-v15";
+var CACHE_NAME = "gtm-rdo-v16";
 var APP_SHELL = [
   "./",
   "./index.html",
